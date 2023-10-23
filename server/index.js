92852cc4f571c73d8b72df495c2df3db6d4ef333
MrM250 <a22miqdervil@inspedralbes.cat>
2023-10-23 12:48:29 +0200

    consultes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ var con = mysql.createConnection({
     database: "a22osczapmar_globalmarket"
 })
 
-
 const port = 3000;
 app.use(express.json())
 app.use(cors())
@@ -41,6 +40,32 @@ function tancarBD() {
     })
 }
 
-app.get('/', (req, res) => {
-    
-  });
+app.get('/consultarUsuaris', (req, res) => {
+    connectarBD()
+    con.query("SELECT * FROM usuaris", function (err, usuaris, fields) {
+        if (err) throw err;
+        usuarisEnviar = []
+        usuaris.forEach(usuari => {
+            usuariIndividual = {id: usuari.id, contrasenya: usuari.contrasenya, nom: usuari.nom, cognoms: usuari.cognoms, email: usuari.email}
+            usuarisEnviar.push(usuariIndividual)
+        })
+
+        res.json(usuarisEnviar)
+                
+    })
+});
+
+app.get('/consultarProductes', (req, res) => {
+    connectarBD()
+    con.query("SELECT productes.*, categorias.nom AS catNom FROM productes JOIN categorias ON productes.id_categoria=categorias.id", function (err, productes, fields) {
+        if (err) throw err;
+        productesEnviar = []
+        productes.forEach(producte => {
+            producteIndividual = {id: producte.id, nom: producte.nom, descripcio: producte.descripcio, preu: producte.preu, quantitat: producte.quantitat, imatge: producte.imatge, id_categoria: producte.id_categoria, nom_categoria: producte.catNom}
+            productesEnviar.push(producteIndividual)
+        })
+
+        res.json(productesEnviar)
+                
+    })
+});
