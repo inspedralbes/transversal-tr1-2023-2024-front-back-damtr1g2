e5edd12938b243142d3bc309fd2b31159d8838ac
osczapmar <a22osczapmar@inspedralbes.cat>
2023-10-27 08:29:54 +0200

    cambios

--- web/src/components/Content.vue
+++ web/src/components/Content.vue
@@ -21,20 +21,20 @@
     <!-- Your main content goes here -->
 
   </div>
-  <v-container >
-    <v-responsive class="align-center text-center fill-height">
+  <v-container class="fill-height container">
+    <v-responsive class=" text-center fill-height">
 
       <div v-if="currentNavItem === 'Comandas'">
-        <v-app-bar app>
-  <v-toolbar-title>Comandas Top Bar</v-toolbar-title>
-  <v-spacer></v-spacer>
+        <v-app-bar app class="filterBar">
+          <v-toolbar-title>Comandas Top Bar</v-toolbar-title>
+          <v-spacer></v-spacer>
 
-  <v-btn text @click="filterByStatus('pendents')">Pendents</v-btn>
-  <v-btn text @click="filterByStatus('en-progres')">En Progrès</v-btn>
-  <v-btn text @click="filterByStatus('completades')">Completades</v-btn>
-</v-app-bar>
+          <v-btn text @click="filterByStatus('pendents')">Pendents</v-btn>
+          <v-btn text @click="filterByStatus('en-progres')">En Progrès</v-btn>
+          <v-btn text @click="filterByStatus('completades')">Completades</v-btn>
+        </v-app-bar>
 
-        <v-row class="fill-height">
+        <v-row class="fill-height comandas-row">
           <v-col cols="9">
             <v-card color="blue lighten-2" class="fill-height">
               <v-card-title>Lista de comandas</v-card-title>
@@ -47,7 +47,7 @@
             </v-card>
           </v-col>
           <v-col cols="3">
-            <v-card color="green lighten-2" class="fill-height">
+            <v-card color="green lighten-2" class="action-panel fill-height">
               <v-card-title>Accions</v-card-title>
               <v-card-text class="d-flex flex-column">
                 <v-btn @click="handleAccept">Acceptar</v-btn>
@@ -57,26 +57,26 @@
             </v-card>
           </v-col>
         </v-row>
+        
       </div>
       <div v-if="currentNavItem === 'Productes'" id="productes">
-      <v-row class="fill-height">
+        <v-row class="fill-height">
           <v-col cols="9">
             <v-card color="	antiquewhite " class="prods">
               <v-btn class="afegirProd" @click="mostrarDialogo('addDialog')">Afegir Nou Producte</v-btn>
               <v-card-title>Lista de productes</v-card-title>
-                <v-card v-for="(producte, index) in productes" :key="index" color="	antiquewhite " class="mb-3">
-                  <v-card-title >{{ producte.nom }}</v-card-title>
-                  <v-img :src="producte.imatge" width="150px" height="auto"></v-img>
-                  <v-btn @click="UpdateData">Actualitzar</v-btn>
-                  <v-btn @click="DeleteData">Esborrar</v-btn>
-                </v-card>
+              <v-card v-for="(producte, index) in productes" :key="index" color="	antiquewhite " class="mb-3">
+                <v-card-title>{{ producte.nom }}</v-card-title>
+                <v-img :src="producte.imatge" width="150px" height="auto"></v-img>
+                <v-btn @click="UpdateData">Actualitzar</v-btn>
+                <v-btn @click="DeleteData">Esborrar</v-btn>
+              </v-card>
             </v-card>
           </v-col>
           <v-col cols="3">
-            
+
           </v-col>
         </v-row>
-
         <v-dialog :class="claseDialog" v-model="dialogVisible">
           <v-form v-model="valido">
           <v-card v-if="claseDialog === 'addDialog'">
@@ -107,6 +107,7 @@
 
 <script>
 import * as funcionesCM from '@/communicationsManager.js';
+import { VWindow } from 'vuetify/lib/components/index.mjs';
 export default {
 
 
@@ -137,15 +138,15 @@ export default {
     };
   },
   async created() {
-    
-        try {
-          this.productes = await funcionesCM.getProductes();
-          console.log(this.productes);
-          console.log("Productos recibidos correctamente")
-        } catch (error) {
-          console.error('Error fetching questions:', error);
-        }
-      },
+
+    try {
+      this.productes = await funcionesCM.getProductes();
+      console.log(this.productes);
+      console.log("Productos recibidos correctamente")
+    } catch (error) {
+      console.error('Error fetching questions:', error);
+    }
+  },
   methods: {
     selectNavItem(item) {
       this.currentNavItem = item;
@@ -172,7 +173,7 @@ export default {
           imatge: this.addInfo.imatge,
           id_categoria: this.addInfo.id_categoria
         }
-        funcionesCM.addPregunta(this.addInfo)
+        funcionesCM.addProducto(this.addInfo)
       } catch {
         console.log('No ha sido posible añadir la información')
       }
@@ -189,12 +190,38 @@ export default {
   .descripcion {
     height: 100px;
   }
-  .afegirProd {
-    top:15px;
-    margin: 10px;
-    position: relative;
-  }
-  .appbar_buttons {
-    margin: 5px;
-  }
+.afegirProd {
+  top: 15px;
+  margin: 10px;
+  position: relative;
+}
+
+.appbar_buttons {
+  margin: 5px;
+}
+
+.container {
+max-width: 100vw;
+}
+
+.filterBar{
+  position: relative !important;
+  
+}
+.comandas-row{
+  margin-top:10vh;
+  position: relative;
+}
+
+.action-panel{
+  height: min-content !important;
+  position: fixed !important;
+  transform: translateX(50%);
+
+}
+
+.v-container{
+  padding: 0;
+}
+
 </style>
