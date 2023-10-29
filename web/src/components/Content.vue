d1066f13db8bb0eb79c02d86573b245ec49ee6c9
osczapmar <a22osczapmar@inspedralbes.cat>
2023-10-29 20:08:48 +0100

    Cambios conexión a socket

--- web/src/components/Content.vue
+++ web/src/components/Content.vue
@@ -62,9 +62,15 @@
                   <v-card-text>Comanda {{ comanda.id }}</v-card-text>
                   
                   <v-btn variant="plain" @click="mostrarDatosComanda(comanda.id)">Datos Comanda</v-btn>
+<<<<<<< Updated upstream
                   <v-btn v-if="selectedFilter === 0" @click="aceptarComanda()">Aceptar</v-btn>
                   <v-btn v-if="selectedFilter === 0" @click="rechazarComanda()">Rechazar</v-btn>
                   <v-btn v-if="selectedFilter === 1" @click="pedidoListo()">Preparado para recoger</v-btn>
+=======
+                  <v-btn v-if="selectedFilter === 0" @click="aceptarComanda(comanda.id)">Aceptar</v-btn>
+                  <v-btn v-if="selectedFilter === 0" @click="rechazarComanda(comanda.id)">Rechazar</v-btn>
+                  <v-btn v-if="selectedFilter === 1" @click="prepararComanda(comanda.id)">Preparado para recoger</v-btn>
+>>>>>>> Stashed changes
                 </v-card>
               </v-card-text>
             </v-card>
@@ -169,6 +175,10 @@
 </template>
 
 <script>
+<<<<<<< Updated upstream
+=======
+const socket = io();
+>>>>>>> Stashed changes
 import * as funcionesCM from '@/communicationsManager.js';
 import { VWindow } from 'vuetify/lib/components/index.mjs';
 export default {
@@ -255,6 +265,18 @@ export default {
       this.filteredComandas =  this.comandas.filter(comanda => comanda.estado === status)
       }
     },
+<<<<<<< Updated upstream
+=======
+    aceptarComanda(id) {
+      socket.emit('aceptarComanda', {comandaId: id})
+    },
+    rechazarComanda(id) {
+      socket.emit('rechazarComanda', {comandaId: id})
+    },
+    prepararComanda(id) {
+      socket.emit('prepararComanda', {comandaId: id})
+    },
+>>>>>>> Stashed changes
     mostrarDialogo(dialogClass, producteId) {
       
       console.log(`ID del producto a editar: `, producteId)
