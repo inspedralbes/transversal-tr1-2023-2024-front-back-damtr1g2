106d1f333182b230aa1e44faffc26c96fa01a26e
osczapmar <a22osczapmar@inspedralbes.cat>
2023-11-03 12:52:39 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -739,13 +739,13 @@ app.post('/actualitzarUsuari', (req, res) => {
     dades = (req.body)
     comprovacio = true
 
-    con.query(`UPDATE usuario SET nom = "${dades.nom}", cognoms = "${dades.cognoms}" WHERE id= ${dades.id}`, function (err, emails, fields) {
+    con.query(`UPDATE usuario SET nom = "${dades.nom}", cognoms = "${dades.cognoms}" WHERE id= ${dades.id}`, function (err, result, fields) {
         if (err) {
             console.log("No s'ha pogut completar l'acció")
             throw err;
         }
         else {
-            console.log("Usuario actualizado correctamente")
+            console.log("Usuario actualizado correctamente: ", result)
         }
         tancarBD()
     }) 
