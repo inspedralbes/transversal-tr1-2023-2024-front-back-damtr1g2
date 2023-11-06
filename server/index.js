6ee0a842c284ed8b37111c2d585c461022a22da2
a22marramgar <121021923+a22marramgar@users.noreply.github.com>
2023-11-06 16:53:11 +0100

    Headers quitados

--- server/index.js
+++ server/index.js
@@ -48,11 +48,6 @@ app.use(express.json())
 
 app.use(Middleware);
 app.use(cors(corsOptions));
-app.all('*', function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
 io.engine.use(Middleware);
 
 server.listen(port, () => {
